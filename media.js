22781a6705aa73fa19b3fefc398dca355962da60
GabrielBento299 <gabrieljesus100-2010@hotmail.com>
2021-12-03 16:17:49 -0300

    first commit

---
+++ media.js
@@ -0,0 +1,73 @@
+const form = document.querySelector('#formulario');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const inputPeso = e.target.querySelector('#peso');
+        const inputAltura = e.target.querySelector('#altura');
+        console.log(inputPeso, inputAltura);
+       
+        const peso = Number(inputPeso.value);
+        const altura = Number(inputAltura.value);
+      
+
+        if (!peso) {
+            setResultado('Peso invalido', false);
+            return;
+        }
+
+        if (!altura) {
+            setResultado('Altura invalida', false);
+            return;
+        }
+        const imc = getImc(peso, altura);
+        const nivelimc = getNivelImc(imc);
+
+        const msg = `Seu IMC É ${imc} (${nivelimc}).`;
+
+        setResultado(msg, true);
+    });
+
+    function getNivelImc (imc, nivelimc) {
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+        
+        if (imc >= 39.) return nivel[5];
+
+        if (imc >= 34.9) return nivel[4];
+
+        if (imc >= 29.9) return nivel[3];
+
+        if (imc >= 24.9) return nivel[2];
+
+        if (imc >= 18.5) return nivel[1];
+
+        if (imc < 18.5) return nivel[0];
+    } 
+
+    function getImc (peso, altura) {
+        const imc = peso / altura **2;
+        return imc.toFixed(2);
+    }   
+
+    function criaP () {
+        const p = document.createElement('p');
+        return p;
+    }
+
+    function setResultado (msg, isvalid) {
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = '';
+
+        
+        const p = criaP();
+
+        if (isvalid) {
+            p.classList.add('paragrafo-resultado');
+
+        } else {
+            p.classList.add('bad');
+        }
+
+        p.innerHTML = msg;
+        resultado.appendChild(p);
+}
